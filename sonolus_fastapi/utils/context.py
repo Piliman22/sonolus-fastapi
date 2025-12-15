71a65261c74baaa66106bb57fed2301165a9619e
Piliman22 <piliman.sab@gmail.com>
2025-12-15 23:53:32 +0900

    user_handleをuser_sessionに変更

--- sonolus_fastapi/utils/context.py
+++ sonolus_fastapi/utils/context.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel
 from typing import Optional
 
 class SonolusContext(BaseModel):
-    user_handle: Optional[str] = None
+    user_session: Optional[str] = None
     is_dev: bool = False
